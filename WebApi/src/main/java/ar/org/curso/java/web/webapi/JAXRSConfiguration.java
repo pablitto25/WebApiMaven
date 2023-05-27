86c8e8af6e9df9903427de4ea91070376d5e3f2a
Pablo Cortes <47508769+pablitto25@users.noreply.github.com>
2023-05-27 01:12:12 -0300

    Proyecto Maven Example

---
+++ WebApi/src/main/java/ar/org/curso/java/web/webapi/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package ar.org.curso.java.web.webapi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
